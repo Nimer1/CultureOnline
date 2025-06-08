4ac27d825dfadc091a5d59deb5092e7a36c46d27
Nimer <nihernandezca@est.utn.ac.cr>
2025-06-07 19:22:35 -0600

    Inicializando proyecto .NET Core MVC en GitLab

---
+++ MVCCultureOnline/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCCultureOnline.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ MVCCultureOnline/Models/LoginViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MVCCultureOnline.Models
+{
+    public class LoginViewModel
+    {
+        public string user {  get; set; }
+        public string password { get; set; }
+    }
+}
