440fe0c7f1b064dc3b257c38055694a26c0efdd8
Nimer <nihernandezca@est.utn.ac.cr>
2025-06-15 19:17:04 -0600

    Se configuro avance 2

---
+++ CultureOnline.Infraestructure/Repository/Implementations/RepositoryAutor.cs
@@ -0,0 +1,34 @@
+﻿using CultureOnline.Infraestructure.Data;
+using CultureOnline.Infraestructure.Repository.Interfaces;
+using CultureOnline.Infraestructure.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CultureOnline.Infraestructure.Repository.Implementations
+{
+    public class RepositoryAutor : IRepositoryAutor
+    {
+        private readonly CultureOnlineContext _context;
+
+        public RepositoryAutor(CultureOnlineContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ICollection<Autor>> ListAsync()
+        {
+            return await _context.Set<Autor>().ToListAsync();
+        }
+
+        public async Task<Autor> FindByIdAsync(int id)
+        {
+            return await _context.Set<Autor>().FindAsync(id);
+        }
+    }
+}
+
