35e82eee86efa925035c5e2cce538c5dd42a77c4
Nimer <nihernandezca@est.utn.ac.cr>
2025-07-14 11:54:25 -0600

    Mantenimiento de reseñas casi terminado

--- CultureOnline.Infraestructure/Models/Productos.cs
+++ CultureOnline.Infraestructure/Models/Productos.cs
@@ -7,7 +7,7 @@ public partial class Productos
 {
     public int Id { get; set; }
 
-    public string Nombre { get; set; } = null!;
+    public string Nombre { get; set; } =  null!;
 
     public string? Descripcion { get; set; }
 
